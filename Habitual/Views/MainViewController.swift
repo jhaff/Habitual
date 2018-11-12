f3370cf0cbcd0f0e0a9b4001e21f2bc620d6e575
jhaff <jacobhaff2@gmail.com>
2018-11-12 14:23:08 -0800

    Created ViewController and Xib files, organized folder structure

---
+++ Habitual/Views/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Habitual
+//
+//  Created by Jacob Haff on 11/12/18.
+//  Copyright © 2018 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
