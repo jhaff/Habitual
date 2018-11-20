fd9efe36b5bb7e50dcdbcb50936a857713290c57
jhaff <jacobhaff2@gmail.com>
2018-11-20 15:44:41 -0800

    Using habit struct instead of strings

---
+++ Habitual/Habit.swift
@@ -0,0 +1,15 @@
+//
+//  Habit.swift
+//  Habitual
+//
+//  Created by Jacob Haff on 11/19/18.
+//  Copyright © 2018 Jacob Haff. All rights reserved.
+//
+
+import Foundation
+
+struct Habit {
+    
+    var title: String
+    
+}
--- Habitual/Views/HabitsTableViewController.swift
+++ Habitual/Views/HabitsTableViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class HabitsTableViewController: UITableViewController {
     var names: [String] = ["Alan", "Adriana", "Adam", "Anne", "Mitchell", "Dani"]
+    
+    var habits: [Habit] = [
+        Habit(title: "Go to bed before 10p"),
+        Habit(title: "Drink 8 Glasses of Water"),
+        Habit(title: "Commit Today"),
+        Habit(title: "Stand up every Hour")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,20 +27,33 @@ class HabitsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        
+        return habits.count
+        
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         var cell: UITableViewCell
+        
         if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            
             cell = dequeueCell
+            
         } else {
+            
             cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+            
         }
-        let name = names[indexPath.row]
-        cell.textLabel?.text = name
+        
+        let habit = habits[indexPath.row]
+        
+        cell.textLabel?.text = habit.title
         
         return cell
+        
     }
+
     
     /*
     // MARK: - Navigation
@@ -57,8 +77,8 @@ extension HabitsTableViewController {
     }
     
     @objc func pressAddHabit(_ sender: UIBarButtonItem) {
-        names.insert("Hello, World!", at: 0)
-        let topIndexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [topIndexPath], with: .automatic)
+//        names.insert("Hello, World!", at: 0)
+//        let topIndexPath = IndexPath(row: 0, section: 0)
+//        tableView.insertRows(at: [topIndexPath], with: .automatic)
     }
 }
