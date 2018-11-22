916196843b010168bafb541b67518066e9f454bf
jhaff <jacobhaff2@gmail.com>
2018-11-22 15:47:27 -0800

    Expanded Habit Struct and wrote DateExtensions

--- Habitual/Habit.swift
+++ Habitual/Habit.swift
@@ -7,9 +7,21 @@
 //
 
 import Foundation
+import UIKit
 
 struct Habit {
-    
+
     var title: String
+    let dateCreated: Date
+    var selectedImage: UIImage
+    
+    var currentStreak: Int
+    var bestStreak: Int
+    var lastCompletionDate: Date?
+    var numberOfCompletions: Int
+    
+    var hasCompletedForToday: Bool {
+        return lastCompletionDate?.isToday ?? false
+    }
     
 }
